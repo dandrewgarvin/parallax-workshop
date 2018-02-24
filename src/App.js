777a3a53bff1f69b3a52e4e37a93e8a085764257
Andrew Garvin <dandrewgarvin@gmail.com>
2018-02-23 20:02:20 -0700

    prepped for mac reboot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-// import CSSParallax from './CSSParallax/CSSParallax'
-// import StarWars from './StarWars/StarWars'
+import CSSParallax from './CSSParallax/CSSParallax'
+import StarWars from './StarWars/StarWars'
 import MoarParallax from './MoarParallax/MoarParallax'
 
 class App extends Component {
@@ -9,18 +9,18 @@ class App extends Component {
     return (
       <div className="App" 
         // style to be used for the star wars example
-        // style={{background: '#000', height: '100vh'}}
+        style={{background: '#000', height: '100vh'}}
         // style={{overflowY: 'scroll', overflowX: 'hidden', perspective: '2px'}}
       >
         {/* Tis the season! */}
-        {/* <StarWars /> */}
+        <StarWars />
         
         {/* simple css parallax images */}
         {/* <CSSParallax />  */}
 
 
         {/* parallax with javascript. boxes scroll at different speeds */}
-        <MoarParallax />
+        {/* <MoarParallax /> */}
 
       </div>
     );
